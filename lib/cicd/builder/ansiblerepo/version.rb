6992cfb9fcd91b1edb091757544a5b555c9a6033
Christo DeLange <cdelange@amplify.com>
2014-07-28 15:06:29 -0400

    Bump version

--- lib/cicd/builder/ansiblerepo/version.rb
+++ lib/cicd/builder/ansiblerepo/version.rb
@@ -2,7 +2,7 @@ module CiCd
   module Builder
     module AnsibleRepo
       # ansiblerepo-builder version
-      VERSION = "0.1.1"
+      VERSION = "0.2.1"
       MAJOR, MINOR, TINY = VERSION.split('.')
       PATCH = TINY
     end
