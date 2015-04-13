2ecad3db89d225fabb087460cda79270b9c9b842
Christo DeLange <github@dldinternet.com>
2015-04-13 11:48:52 -0400

    Add analyzeInventory, syncInventory, syncRepo and pruneInventory to maintain repos

--- lib/cicd/builder/ansiblerepo/version.rb
+++ lib/cicd/builder/ansiblerepo/version.rb
@@ -2,7 +2,7 @@ module CiCd
   module Builder
     module AnsibleRepo
       # ansiblerepo-builder version
-      VERSION = '0.3.7'
+      VERSION = '0.3.8'
       MAJOR, MINOR, TINY = VERSION.split('.')
       PATCH = TINY
     end
