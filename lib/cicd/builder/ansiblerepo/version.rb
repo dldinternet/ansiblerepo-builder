14aba643704d61af3776982c38497aa0e2f099dc
Christo DeLange <github@dldinternet.com>
2015-04-30 14:58:04 -0400

    Fix the artifact extension detection

--- lib/cicd/builder/ansiblerepo/version.rb
+++ lib/cicd/builder/ansiblerepo/version.rb
@@ -2,7 +2,7 @@ module CiCd
   module Builder
     module AnsibleRepo
       # ansiblerepo-builder version
-      VERSION = '0.4.4'
+      VERSION = '0.4.5'
       MAJOR, MINOR, TINY = VERSION.split('.')
       PATCH = TINY
     end
