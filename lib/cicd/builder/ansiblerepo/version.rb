08e1715a000341ecc4479390256bc5da5857d3bd
Christo DeLange <github@dldinternet.com>
2015-05-05 11:52:10 -0400

    Fix .checsum key= in CiCd builder

--- lib/cicd/builder/ansiblerepo/version.rb
+++ lib/cicd/builder/ansiblerepo/version.rb
@@ -2,7 +2,7 @@ module CiCd
   module Builder
     module AnsibleRepo
       # ansiblerepo-builder version
-      VERSION = '0.4.9'
+      VERSION = '0.5.0'
       MAJOR, MINOR, TINY = VERSION.split('.')
       PATCH = TINY
     end
